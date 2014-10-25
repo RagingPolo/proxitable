12125cb9b2763ca41eaff4aea9e645a68bd87430
MadLarkin <fething_mad_larkin@hotmail.com>
2014-10-25 17:11:33 +0100

    Modified Citadel.py to use external output objects

--- citadel/Citadel.py
+++ citadel/Citadel.py
@@ -1,24 +1,19 @@
 # -----------------------------------------------------------------
 # Citidel game - written in python 3                              |
 #                                                                 |
-# Requires lib 'colorama' install with pip: pip3 install colorama |
-#                                                                 |
 # To run game: python3 path/to/Citadel.py                         |
-#                                                                 |
-# Simple ASCII output provided for testing, GPIO output method    |
-# is currently a stub to be finished when electronics are known   |
 # -----------------------------------------------------------------
+import numbers
+import getpass
+import CitOutAbstract
+# Import desired output class here
+#import ???
 
 # ------------------------------------
 # CLASS CitPlayer                    |
 #                                    |
 # Maintains state of a single player |
 # ------------------------------------
-import os
-import numbers
-import getpass
-from colorama import init, Fore
-
 class CitPlayer( object ):
   
   def __init__( self ):
@@ -91,49 +86,39 @@ class CitBoard( object ):
 # ------------------------------------
 class CitGame( object ):
 
-  OUTPUT = [ 'ASCII', 'GPIO' ]
-
   def __init__( self ):
-    init() # Colorama init
-    self._board = CitBoard()
-    self._p1 = CitPlayer()
-    self._p2 = CitPlayer()
-    self._output = CitGame.OUTPUT.index( 'ASCII' )
+    self.__board = CitBoard()
+    self.__p1 = CitPlayer()
+    self.__p2 = CitPlayer()
+    self.__output = None
   
   def run( self ):
     winner = self.hasWinner()
     while winner == 0:
-      self.display()
+      self.showState()
       self.move( getMove( 'Player 1' ), getMove( 'Player 2' ) )
       winner = self.hasWinner()
-    self.display()
-    if winner == 1:
-      print( '          Player 1 Wins!' )
-    elif winner == 2:
-      print( '          Player 2 Wins!' )
-    else:
-      print( '               Draw' )
-    print()
+    self.showState()
+    self.showResult()
 
   def setOutput( self, output ):
-    try:
-      x = CitGame.OUTPUT.index( output )
-      self._output = x
-    except ValueError as e:
-      pass
-    
+    if isinstance( output, CitOutAbstract ):
+      self.__output = output
+    else:
+      raise Exception( 'CitGame.setOutput(): Not a valid output object' )
+
   # Play out a turn of the game
   # p1m : Player ones move
   # p2m : Player twos move
   def move( self, p1m, p2m ):
     if ( isinstance( p1m, numbers.Number ) and
          isinstance( p2m, numbers.Number ) ):
-      self._p1.addMove( p1m )
-      self._p2.addMove( p2m )
+      self.__p1.addMove( p1m )
+      self.__p2.addMove( p2m )
       if p1m > p2m:
-        self._board.moveRight()
+        self.__board.moveRight()
       elif p2m > p1m:
-        self._board.moveLeft()
+        self.__board.moveLeft()
     else:
       raise Exception( 'CitGame.move(): Not a number' );
 
@@ -143,71 +128,42 @@ class CitGame( object ):
   #         2 : player two
   #         3 : draw
   def hasWinner( self ):
-    if self._board.getPosition() == CitBoard.MAX:
+    if self.__board.getPosition() == CitBoard.MAX:
       return 1
-    if self._board.getPosition() == CitBoard.MIN:
+    if self.__board.getPosition() == CitBoard.MIN:
       return 2
-    if self._p1.hasLost():
-      if self._p2.canWin( self._board.getPosition() ):
+    if self.__p1.hasLost():
+      if self.__p2.canWin( self.__board.getPosition() ):
         while self._board.getPosition() > CitBoard.MIN:
-          self._p2.addMove( 1 )
-          self._board.moveLeft()
+          self.__p2.addMove( 1 )
+          self.__board.moveLeft()
         return 2
       else:
         return 3
-    if self._p2.hasLost():
-      if self._p1.canWin( CitBoard.MAX - self._board.getPosition() ):
-        while self._board.getPosition() < CitBoard.MAX:
-          self._p1.addMove( 1 )
-          self._board.moveRight()
+    if self.__p2.hasLost():
+      if self.__p1.canWin( CitBoard.MAX - self.__board.getPosition() ):
+        while self.__board.getPosition() < CitBoard.MAX:
+          self.__p1.addMove( 1 )
+          self.__board.moveRight()
         return 1
       else:
         return 3
     return 0
 
-  def display( self ):
-    if self._output == CitGame.OUTPUT.index( 'ASCII' ):
-      self._asciiDisplay()
-    elif self._output == CitGame.OUTPUT.index( 'GPIO' ):
-      self._gpioDisplay()
+  def showState( self ):
+    if self.__output is not None:
+      self.__output.showState()
 
-  def _asciiDisplay( self ):
-    os.system( 'cls' if os.name == 'nt' else 'clear' ) 
-    print( Fore.CYAN + '   P1[' + Fore.RED + '{0:2d}'.format( self._p1.getPoints() ) + Fore.CYAN + ']' + Fore.WHITE +
-           '-->         <--' + Fore.CYAN + '[' + Fore.RED + '{0:2d}'.format( self._p2.getPoints() ) + Fore.CYAN + ']P2' )
-    print( Fore.YELLOW + '                |' )
-    print( '            | - | - |' )
-    print( '        |- -|- -|- -|- -|' )
-    pos = self._board.getPosition()
-    if pos == 0:
-      print( ' --[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]-[ ]-[ ]-[ ]-[ ]-[ ]--' )
-    elif pos == 1:
-      print( ' --[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]-[ ]-[ ]-[ ]-[ ]--' )
-    elif pos == 2:
-      print( ' --[ ]-[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]-[ ]-[ ]-[ ]--' )
-    elif pos == 3:
-      print( ' --[ ]-[ ]-[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]-[ ]-[ ]--' )
-    elif pos == 4:
-      print( ' --[ ]-[ ]-[ ]-[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]-[ ]--' )
-    elif pos == 5:
-      print( ' --[ ]-[ ]-[ ]-[ ]-[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']-[ ]--' )
-    elif pos == 6:
-      print( ' --[ ]-[ ]-[ ]-[ ]-[ ]-[ ]-[' + Fore.RED + 'x' + Fore.YELLOW + ']--' )
-    print( '        |- -|- -|- -|- -|' )
-    print( '            | - | - |' )
-    print( '                |' + Fore.RESET )
-    print()
-
-  def _gpioDisplay( self ):
-    pass
-
+  def showResult( self ):
+    if self.__output is not None:
+      self.__output.showResult()
 # ------------------------------------
 
 # Get move from human
 def getMove( name ):
   success = False
   while not success:
-    move = getpass.getpass( Fore.WHITE + ' ' + name + ' enter move: ' + Fore.RESET )
+    move = getpass.getpass( ' ' + name + ' enter move: ' )
     try:
       int_move = int( move )
       success = True
@@ -217,5 +173,4 @@ def getMove( name ):
 
 # Main -------------------------------
 game = CitGame()
-#game.setOuput( 'GPIO' )
 game.run()
