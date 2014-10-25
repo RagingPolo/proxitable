a72adba002d083f0b88c0f67752228cc6d1d14b6
MadLarkin <fething_mad_larkin@hotmail.com>
2014-10-26 00:16:09 +0100

    Created abstract input class

---
+++ citadel/CitInAbstract.py
@@ -0,0 +1,19 @@
+from abc import ABCMeta, abstractmethod
+# ------------------------------------
+# ABSTRACT CLASS CitIn               |
+#                                    |
+# Defines required method for a      |
+# valid input class.                 |
+# ------------------------------------
+class CitInAbstract:
+  __metaclass__ = ABCMeta
+
+  # Get a players input, the method must
+  # RETURN a positive integer value no
+  # greater than points
+  # name   : name of player entering move
+  # points ; players current points total
+  # last   : opponents last move ( to aid AI players )
+  @abstractmethod
+  def getMove( self, name, points, last=None ):
+    pass
