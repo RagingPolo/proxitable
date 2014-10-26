56338752d4a486ac723517a2efe1f030ff2c63bd
MadLarkin <fething_mad_larkin@hotmail.com>
2014-10-26 01:14:25 +0100

    Added support for player specific input modules

--- citadel/CitGame.py
+++ citadel/CitGame.py
@@ -1,8 +1,8 @@
 import CitPlayer as CP
 import CitBoard as CB
 import CitOutAbstract
+import CitInAbstract
 import numbers
-import getpass
 # ------------------------------------
 # CLASS CitGame                      |
 #                                    |
@@ -12,19 +12,32 @@ import getpass
 class CitGame( object ):
 
   def __init__( self ):
+    self.P1 = 1
+    self.P2 = 2
     self.__board = CB.CitBoard()
-    self.__p1 = CP.CitPlayer()
-    self.__p2 = CP.CitPlayer()
+    self.__player = { self.P1 : CP.CitPlayer(), self.P2 : CP.CitPlayer() }
     self.__output = None
-  
+    self.__input = { self.P1 : None, self.P2 : None }
+
   def run( self ):
     winner = self.hasWinner()
     while winner == 0:
       self.showState()
-      self.move( getMove( 'Player 1' ), getMove( 'Player 2' ) )
+      self.move( self.getMove( self.P1 ), getMove( self.P2 ) )
       winner = self.hasWinner()
     self.showState()
     self.showResult()
+  
+  # Set the desired move input module for the specified player ( P1 | P2 )
+  def setInput( self, player, input_ ):
+    if isinstance( input_, CitInAbstract.CitInAbstract ):
+      try:
+        self.__input[ player ] = input_
+      except KeyError:
+        raise Exception( 'CitGame.setInput(): Not a valid player, use P1 - 1 or P2 - 2' )
+    else:
+      raise Exception( 'CitGame.setInput(): Not a valid input object' )
+      
 
   def setOutput( self, output ):
     if isinstance( output, CitOutAbstract.CitOutAbstract ):
@@ -38,8 +51,9 @@ class CitGame( object ):
   def move( self, p1m, p2m ):
     if ( isinstance( p1m, numbers.Number ) and
          isinstance( p2m, numbers.Number ) ):
-      self.__p1.addMove( p1m )
-      self.__p2.addMove( p2m )
+      # AddMove() must not be called until both players moves have been recieved
+      self.__player[ 1 ].addMove( p1m )
+      self.__player[ 2 ].addMove( p2m )
       if p1m > p2m:
         self.__board.moveRight()
       elif p2m > p1m:
@@ -57,45 +71,54 @@ class CitGame( object ):
       return 1
     if self.__board.getPosition() == CB.CitBoard.MIN:
       return 2
-    if self.__p1.hasLost():
-      if self.__p2.canWin( self.__board.getPosition() ):
+    if self.__player[ 1 ].hasLost():
+      if self.__player[ 2 ].canWin( self.__board.getPosition() ):
         while self.__board.getPosition() > CB.CitBoard.MIN:
-          self.__p2.addMove( 1 )
+          self.__player[ 2 ].addMove( 1 )
           self.__board.moveLeft()
         return 2
       else:
         return 3
-    if self.__p2.hasLost():
-      if self.__p1.canWin( CB.CitBoard.MAX - self.__board.getPosition() ):
+    if self.__player[ 2 ].hasLost():
+      if self.__player[ 1 ].canWin( CB.CitBoard.MAX - self.__board.getPosition() ):
         while self.__board.getPosition() < CB.CitBoard.MAX:
-          self.__p1.addMove( 1 )
+          self.__player[ 1 ].addMove( 1 )
           self.__board.moveRight()
         return 1
       else:
         return 3
     return 0
 
+  # Get a move for the specified player using the players input module
+  def getMove( self, player ):
+    # Calculate opponent from player value, 
+    opponent = 3 # if player is invalid opponent will stay invalid as 3
+    if player == self.P1:
+      opponent = self.P2
+    elif player == self.P2:
+      opponent == self.P1
+    # Try and call the players input module
+    try:
+      if self.__input[ player ] is not None:
+        # TODO add name attribute and associated methods to CitPlayer class
+        self.__input[ player ].getMove( str( player ),
+                                        self.__player[ player ].getPoints(),
+                                        self.__player[ opponent ].getLastMove() )
+      else:
+        # TODO is there a way to make this an uncatchable fatal error?
+        raise Exception( 'CitGame.getMove(): Fatal error - No input module provided for ' + str( player ) )
+    except KeyError:
+      raise Exception( 'CitGame.getMove(): Not a valid player, use P1 - 1 or P2 - 2' )
+
   def showState( self ):
     if self.__output is not None:
       self.__output.showState( self.__board.getPosition(),
-                               self.__p1.getPoints(), 
-                               self.__p2.getPoints(),
-                               self.__p1.getLastMove(),
-                               self.__p2.getLastMove() )
+                               self.__player[ 1 ].getPoints(), 
+                               self.__player[ 2 ].getPoints(),
+                               self.__player[ 1 ].getLastMove(),
+                               self.__player[ 2 ].getLastMove() )
 
   def showResult( self ):
     if self.__output is not None:
       self.__output.showResult( self.hasWinner() )
 # ------------------------------------
-
-# Get move from human
-def getMove( name ):
-  success = False
-  while not success:
-    move = getpass.getpass( ' ' + name + ' enter move: ' )
-    try:
-      int_move = int( move )
-      success = True
-    except:
-      pass
-  return int_move
