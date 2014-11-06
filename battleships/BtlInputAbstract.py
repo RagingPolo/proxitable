ce21e2ba72df0655bd9415db17674b61e2c65cbc
MadLarkin <fething_mad_larkin@hotmail.com>
2014-11-06 21:40:41 +0000

    Created abstract input class

---
+++ battleships/BtlInputAbstract.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+# ------------------------------------
+# ABSTRACT CLASS BtlInputAbstract    |
+#                                    |
+# Provides a players move for each   |
+# turn of the game                   |
+# ------------------------------------
+class BtlInputAbstract( object ):
+  
+  # Record if this is an ai player input
+  # All sub classes should call super.__init__( ai )
+  def __init__( self, ai ):
+    self.__ai = ai
+
+  # Get a players move for the turn
+  # must RETURN a valid BtlMove
+  # object
+  @abstractmethod
+  def getMove( self ):
+    pass
+
+  def isAi( self ):
+    if self.__ai is True:
+      return True
+    return False
+# ------------------------------------
