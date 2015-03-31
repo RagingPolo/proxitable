3e14f60b13ed83419d288f3c21569fb6fe902bfd
MadLarkin <fething_mad_larkin@hotmail.com>
2015-03-31 22:19:03 +0100

    More citadel porting

--- javascript/citadel/citadel.js
+++ javascript/citadel/citadel.js
@@ -1,4 +1,113 @@
 /******************************************************************************
+ * Contains game logic and maintains overall game state                      */
+function Citadel() {
+  this.board = new CitBoard();
+  this.player = { 1 : new CitPlayer( 'Player 1' ),
+                  2 : new CitPlayer( 'Player 2' ) };
+  this.output = CitOutput();
+  this.input  = { 1 : new CitInPES(),
+                  2 : new CitInBot() };
+}
+/* Main game loop */
+Citadel.prototype.run() {
+  var winner = this.hasWinner();
+  while ( 0 == winner ) {
+    this.output.showState( this.board.getPosition(),
+                           this.player[ 1 ].getPoints(),
+                           this.player[ 2 ].getPoints(),
+                           this.player[ 1 ].getLastMove(),
+                           this.player[ 2 ].getLastMove() );
+    this.getMoves();
+    winner = this.hasWinner();
+  }
+  this.output.showState( this.board.getPosition(),
+                         this.player[ 1 ].getPoints(),
+                         this.player[ 2 ].getPoints(),
+                         this.player[ 1 ].getLastMove(),
+                         this.player[ 2 ].getLastMove() );
+  this.output.showResult( this.hasWinner() );
+};
+/* Get both players moves */
+Citadel.prototype.getMoves() {
+  var p1m = this.input[ 1 ].getMove( this.player[ 1 ].getPoints,
+                                     this.player[ 2 ].getLastMove() );
+  var p2m = this.input[ 2 ].getMove( this.player[ 2 ].getPoints,
+                                     this.player[ 1 ].getLastMove() );
+  this.player[ 1 ].addMove( p1m );
+  this.player[ 2 ].addMove( p2m );
+  if ( p1m > p2m ) {
+    this.board.moveRight();
+  } else if ( p2m > p1m ) {
+    this.board.moveLeft();
+  } 
+}
+/* Check if there is a winner
+ * returns - 0:not finished, 1:player 1, 2:player 2, 3:draw */
+Citadel.prototype.hasWinner() {
+  if ( this.board.getPosition() == this.board.MAX ) return 1;
+  if ( this.board.getPosition() == this.board.MIN ) return 2;
+  if ( this.player[ 1 ].hasLost() ) {
+    if ( this.player[ 2 ].canWin( this.board.getPosition() ) ) {
+      while ( this.board.getPosition() > this.board.MIN ) {
+        this.player[ 2 ].addMove( 1 );
+        this.board.moveLeft();
+      }
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+  if ( this.player[ 2 ].hasLost() ) {
+    if ( this.player[ 1 ].canWin( this.board.getPosition() ) ) {
+      while ( this.board.getPosition() < this.board.MAX ) {
+        this.player[ 1 ].addMove( 1 );
+        this.board.moveRight();
+      }
+      return 1;
+    } else {
+      return 3;
+    }
+  }
+  return 0;
+};
+/*****************************************************************************/
+
+/******************************************************************************
+ * Manipulate the html based on the current game state                       */
+function CitOutput() {
+  //TODO
+}
+/* Update the board to the current state */
+CitOutput.prototype.showState( pos, p1p, p2p, p1m, p2m ) {
+  //TODO
+};
+/* Display the result once the game has finished */
+CitOutput.prototype.showResult( winner ) {
+  //TODO
+};
+/*****************************************************************************/
+
+/******************************************************************************
+ * Get user input for the keyboard or pes controller                         */
+function CitInPES() {
+  //TODO
+}
+CitInPES.prototype.getMove( points, opLast ) {
+  //TODO
+};
+/*****************************************************************************/
+
+/******************************************************************************
+ * Get the user input from the AI bot                                        */
+function CitInBot() {
+  //TODO
+}
+CitInBot.prototype.getMove( points, opLast ) {
+  //TODO
+};
+/*****************************************************************************/
+
+/******************************************************************************
  * Maintains state of the citadel game board, board consists of 7 positions  */
 function CitBoard() {
   this.MIN = 0; // Lowest board position
