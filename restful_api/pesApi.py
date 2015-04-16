16af6adb9b6287de31f748e803301d0d32ecaa34
MadLarkin <fething_mad_larkin@hotmail.com>
2015-04-16 17:19:28 +0100

    Set status of buttons tested on PES

--- restful_api/pesApi.py
+++ restful_api/pesApi.py
@@ -1,46 +1,30 @@
 from flask import Flask, jsonify, request
 import RPi.GPIO as GPIO
 from time import sleep
-
-# TODO create a dict to map button names, UP, DOWN, etc to pin numbers
-#      so that the pins are abstracted for the client side code
+from flask.ext.cors import cross_origin
 
 app = Flask( __name__ )
 # Relevent pins for the PES hardware
-INPUT_PINS  = [ 12, 23, 18, 21, 26, 7, 10, 15 ]
-OUTPUT_PINS = [ 24, 22, 19, 16, 13, 11, 8, 5 ]
+INPUT_PINS  = { 'START' : 12, 'UP' : 23, 'DOWN' : 18, 'RIGHT' : 21,
+                'LEFT' : 26, 'A' : 7, 'B' : 10, 'SELECT' : 15 }
+OUTPUT_PINS = { 'LEFT' : 24, 'UP' : 22, 'RIGHT' : 19,'DOWN' : 16,
+                'SELECT' : 13, 'START' : 11, 'B' : 8, 'A' : 5 }
 
-@app.route( '/pins', methods = [ 'GET', 'PUT', 'OPTIONS' ] )
+@app.route( '/pins', methods = [ 'GET', 'POST' ] )
+@cross_origin( allow_headers=['Content-Type'] )
 def pins():
-  # Send a response that will allow cross domain requests
-  if request.method == 'OPTIONS':
-    return "", 200, {
-      'Access-Control-Allow-Origins' : '*',
-      'Access-Control-Allow-Methods' : 'PUT,GET',
-      'Access-Control-Allow-Headers' : 'Content-Type'
-    }
-  if request.method == 'GET':
-    # Poll all pins output pins and return status
-    status = {}
-    for pin in OUTPUT_PINS:
-      status[ pin ] = True if GPIO.output( pin ) else False
-    return jsonify( status )
-  if request.method == 'PUT': 
+  status = {}
+  if request.method == 'POST':
     if request.headers[ 'Content-Type' ] == 'application/json':
-      status = request.json
-      # Update all off the output pins
-      for pin, setting in status:
-        if pin in OUTPUT_PINS:
-          # TODO Should have some exception handling in case setting isn't boolean
-          GPIO.output( pin, setting )
-          # TODO Also how do we want the hardware to behave if the user trys to set
-          #      an invalid pin, ignore? bail? undo all changes?
-      # Success - No Content
-      return "", 204, {}
-    # Not json - Unsupported Media Type
-    return "", 415, {}
-  # Bad request - Method Not Allowed
-  return "", 405, {}
+      for button in request.data.decode( 'utf-8' ).split( '&' ):
+        kv = button.split( '=' )      
+        status[ kv[ 0 ] ] = True if kv[ 1 ] == 'true' else False
+      for button, setting in status.items():
+        if button in OUTPUT_PINS:
+          GPIO.output( OUTPUT_PINS[ button ], setting )
+    return "", 204, {}
+  if request.method == 'GET':
+    return "", 204, {}
 
 @app.route( '/pressed', methods = [ 'GET' ] )
 def pressed():
@@ -58,8 +42,8 @@ def pressed():
 if '__main__' == __name__:
   # Setup the GPIO pins
   GPIO.setmode( GPIO.BOARD )
-  for pin in INPUT_PINS:
-    GPIO.setup( pin, GPIO.IN ):
-  for pin in OUTPUT_PINS:
-    GPIO.setup( pin, GPIO.OUT ):
+  for button, pin in INPUT_PINS.items():
+    GPIO.setup( pin, GPIO.IN )
+  for button, pin in OUTPUT_PINS.items():
+    GPIO.setup( pin, GPIO.OUT )
   app.run( host='0.0.0.0', port=8080, threaded=True )
