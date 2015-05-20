3f4bcd58a2ecee35f80c7f4945806d315ab629d5
MadLarkin <fething_mad_larkin@hotmail.com>
2015-05-20 18:39:19 +0100

    Started battleship basic view

---
+++ javascript/battleships/BtlSquare.js
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * Produce the html squares to make up the view of the game                  */
+function BtlSquare() {
+  this.id = '';
+  this.cssClass = '';
+  this.content = '';
+}
+// Rest the square to default values
+BtlSquare.prototype.reset = function() {
+  this.id = '';
+  this.cssClass = '';
+  this.content = '';
+};
+// Add an id to the square
+BtlSquare.prototype.addId = function( id ) {
+  this.id = id;
+};
+// Add a class to the square
+BtlSquare.prototype.addClass = function( cssClass ) {
+  this.cssClass += cssClass + ' ';
+};
+// Add some content to the square
+BtlSquare.prototype.addContent = function( content ) {
+  this.content += content;
+};
+// Get the html for a single square
+BtlSquare.prototype.getHtml = function() {
+  var square = '<div class="square';
+  if ( this.cssClass.len > 0  ) {
+    square += this.cssClass;
+  }
+  square += '"';
+  if ( this.id.len > 0 ) {
+    square += ' id="';
+    square += this.id;
+    square += '"';
+  }
+  square += '><div class="content"><div class="centre">';
+  square += this.content;
+  square += '</div></div></div>';
+  this.reset();
+  return square;
+};
+/*****************************************************************************/
