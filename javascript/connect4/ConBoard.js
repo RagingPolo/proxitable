3499bc7cc162ba7235512da5eb72867fd90ab206
MadLarkin <fething_mad_larkin@hotmail.com>
2015-06-19 16:56:37 +0100

    Fixed random win bug

--- javascript/connect4/ConBoard.js
+++ javascript/connect4/ConBoard.js
@@ -30,49 +30,27 @@ ConBoard.prototype.drop = function( col, player ) {
 };
 // Check if there is a winner
 ConBoard.prototype.winner = function() {
-  var line = 0;
   var player = 0;
   // Check vertically
   for ( var c = 0 ; c < this.width ; ++c ) {
-    for ( var r = 0 ; r < this.height ; ++r ) {
-      if ( 0 == this.board[ c ][ r ] ) {
-        // There are no more pieces in the column
-        player = 0;
-        line = 0;
-        break;
-      }
-      if ( 0 == line ) {
-        // Record the player attempting a line
-        player = this.board[ c ][ r ];
-      } else {
-        if ( player != this.board[ c ][ r ] ) {
-          // It's the other player so reset the player and line counter
-          player = this.board[ c ][ r ];
-          line = 0;
-        }
-      }
-      // increment the line counter and if it equals four we have a winner
-      ++line;
-      if ( 4 == line ) {
+    for ( var r = 0 ; r < this.height - 3 ; ++r ) {
+      if ( ( player = this.match( [ this.board[ c ][ r ],
+                                    this.board[ c ][ r + 1 ],
+                                    this.board[ c ][ r + 2 ],
+                                    this.board[ c ][ r + 3 ] ]
+         ) ) ) {
         return player;
       }
     }
   }
   // We didn't find a winner so check horizontally
-  line = 0;
-  player = 0;
   for ( var r = 0 ; r < this.height ; ++r ) {
-    for ( var c = 0 ; c < this.width ; ++c ) {
-      if ( player != this.board[ c ][ r ] ) {
-        player = this.board[ c ][ r ];
-        line = 0;
-      }
-      // if player is currently an actual player increment
-      // line, if line reaches 4 we have a winner
-      if ( player != 0 ) {
-        ++line;
-      }
-      if ( 4 == line ) {
+    for ( var c = 0 ; c < this.width - 3 ; ++c ) {
+      if ( ( player = this.match( [ this.board[ c ][ r ],
+                                    this.board[ c + 1 ][ r ],
+                                    this.board[ c + 2 ][ r ],
+                                    this.board[ c + 3 ][ r ] ]
+         ) ) ) {
         return player;
       }
     }
